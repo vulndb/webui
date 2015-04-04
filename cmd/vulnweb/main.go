f40c4746d943565fc9b5398c767a69941262b453
Vyacheslav Bakhmutov <m0sth8@gmail.com>
2015-04-04 17:09:31 -0300

    Initial commit for webui

---
+++ cmd/vulnweb/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/russross/blackfriday"
+	"github.com/vulndb/webui"
+)
+
+var vulndbPath = flag.String("vulndb-path", "../data/db", "path to vulndb database")
+var address = flag.String("http-addr", "127.0.0.1:8080", "http address")
+var templates = flag.String("templates", "./templates", "path to templates")
+
+var funcMap = template.FuncMap{
+	"severity": func(value string) string {
+		switch value {
+		case "high":
+			return "danger"
+		case "medium":
+			return "warning"
+		case "low":
+			return "info"
+		}
+		return "default"
+	},
+	"markdown": func(value fmt.Stringer) (template.HTML, error) {
+		return template.HTML(blackfriday.MarkdownCommon([]byte(value.String()))), nil
+	},
+}
+
+type TemplateObj struct {
+	Vulns webui.VulnList
+	Vuln  *webui.Vuln
+	Title string
+	Err   error
+}
+
+func tPath(name string) string {
+	return path.Join(*templates, name+".html")
+}
+
+func renderTemplate(w http.ResponseWriter, tmplName string, obj *TemplateObj) {
+	t, err := template.New(tmplName).Funcs(funcMap).ParseFiles(tPath(tmplName), tPath("base"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	buf := bytes.NewBuffer(nil)
+	err = t.ExecuteTemplate(buf, "base", obj)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	io.Copy(w, buf)
+}
+
+func IndexHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	vulns, err := webui.GetVulns(*vulndbPath)
+	renderTemplate(w, "vulns", &TemplateObj{Err: err, Vulns: vulns})
+}
+
+func VulnHandler(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	idStr := p.ByName("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		renderTemplate(w, "vuln", &TemplateObj{Err: err})
+		return
+	}
+	vulns, err := webui.GetVulns(*vulndbPath)
+	renderTemplate(w, "vuln", &TemplateObj{Err: err, Vuln: vulns.GetById(id)})
+}
+
+func VulnTagHandler(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	tag := p.ByName("tag")
+	vulns, err := webui.GetVulns(*vulndbPath)
+	renderTemplate(w, "vulns", &TemplateObj{
+		Err:   err,
+		Vulns: vulns.FilterByTag(tag),
+		Title: fmt.Sprintf("Filtered by tag '%s'", tag),
+	})
+}
+
+func VulnSeverityHandler(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	severity := p.ByName("severity")
+	vulns, err := webui.GetVulns(*vulndbPath)
+	renderTemplate(w, "vulns", &TemplateObj{
+		Err:   err,
+		Vulns: vulns.FilterBySeverity(severity),
+		Title: fmt.Sprintf("Filtered by severity '%s'", severity),
+	})
+}
+
+func main() {
+	flag.Parse()
+	router := httprouter.New()
+
+	router.GET("/", IndexHandler)
+	router.GET("/vuln/:id", VulnHandler)
+	router.GET("/tag/:tag", VulnTagHandler)
+	router.GET("/severity/:severity", VulnSeverityHandler)
+
+	log.Printf("Listen on %s", *address)
+	if err := http.ListenAndServe(*address, router); err != nil {
+		panic(err)
+	}
+}
